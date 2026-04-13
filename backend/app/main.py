ea5c5462db58fdb04add32c7bc95038aa0705fa8
Natali <shaathnatali@gmail.com>
2026-04-13 19:58:09 +0300

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -9,6 +9,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "https://talbiya-app.vercel.app",
+        "https://talbiya-app-git-main-natalishaats-projects.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
